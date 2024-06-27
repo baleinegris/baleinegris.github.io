891a377956b0c5e2f40d80266383b41ed90f6580
Oscar Heath <136947514+baleinegris@users.noreply.github.com>
2024-06-27 16:48:29 -0400

    added some more UI, opinions, and animations

---
+++ Fun Stuff/tetris.js
@@ -0,0 +1,5 @@
+var grid = [];
+var gridWidth = 10;
+var gridHeight = 20;
+
+var currentPiece = null;
--- app.js
+++ app.js
@@ -2,3 +2,16 @@ var opinionsButton = document.getElementById('opinions');
 opinionsButton.addEventListener('click', function() {
     window.location.href = 'Opinions/opinions.html';
 });
+
+var funStuffButton = document.getElementById('funStuff');
+funStuffButton.addEventListener('click', function() {
+    window.location.href = 'Fun Stuff/tetris.html';
+});
+
+var knight = document.getElementById('KnightGif');
+knight.addEventListener('click', function() {
+    knight.src='knight-attack.gif';
+    setTimeout(function(){
+        knight.src='knight-idle.gif';},
+        750);
+});
